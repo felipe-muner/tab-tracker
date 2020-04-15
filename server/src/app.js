93487d686739c816b41d5472c779806b19e5e2c7
felipemuner <felipe.muner@gmail.com>
2020-04-15 23:25:05 +0700

    set up server expressjs

---
+++ server/src/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send({ tudo: "ok" });
+});
+
+app.listen(process.env.PORT || 8081);

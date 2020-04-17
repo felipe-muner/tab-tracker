ec24cafe94e56ac184d94c1f69ba0ce7d571c64c
felipemuner <felipe.muner@gmail.com>
2020-04-17 19:24:28 +0700

    set up jsonwebtoken

--- server/src/app.js
+++ server/src/app.js
@@ -5,7 +5,11 @@ const { sequelize } = require("./models");
 const config = require("./config/config");
 
 const app = express();
-
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.use(bodyParser.json());
 app.use(cors());
 

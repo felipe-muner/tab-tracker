06120996d8b5ed0022244c22bcd29e677e0d1174
felipemuner <felipe.muner@gmail.com>
2020-04-20 20:19:05 +0700

    set up query string when user type in search field

--- client/src/components/SongsSearchPanel.vue
+++ client/src/components/SongsSearchPanel.vue
@@ -1,7 +1,35 @@
-<template> <div>12321</div></template>
+<template>
+  <div>
+    <Panel title="Search">
+      <v-text-field
+        v-model="search"
+        label="search by song title, artist, album, genre"
+        outlined
+        dense
+      ></v-text-field>
+    </Panel>
+  </div>
+</template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return { search: 10 };
+  },
+  watch: {
+    search(val) {
+      const route = {
+        name: "songs"
+      };
+      if ("" !== this.search) {
+        route.query = {
+          search: this.search
+        };
+        this.$router.push(route);
+      }
+    }
+  }
+};
 </script>
 
 <style></style>
